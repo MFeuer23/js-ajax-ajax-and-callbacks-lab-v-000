4fd77b3311a1094e58b33bbfc28f057ed7b70101
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-09 13:38:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ function searchRepositories() {
   const input = document.getElementById("searchTerms").value
   const url = `https://api.github.com/${input}/repos`
   console.log(input);
+  console.log(url);
 }
