a94134039d98de739072d02a6d3439147533cb43
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-09 13:41:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ $(document).ready(function (){
 function searchRepositories() {
   const input = document.getElementById("searchTerms").value
   const url = `https://api.github.com/${input}/repos`
-  $.get(url)
+  $.get(url).done(function(data) {
+    
+  }).fail(displayError())
+}
+
+function displayError() {
+  "I'm sorry, there's been an error. Please try again."
 }
