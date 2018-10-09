a69f4b87c9a6c0aa66cc06cd216300d9ce65a262
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-09 13:58:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ $(document).ready(function (){
 function searchRepositories() {
   const input = document.getElementById("searchTerms").value
   const url = `https://api.github.com/users/${input}/repos`
-  $.get(url).done(function(data) {
-    console.log(data);
-    $(".result").html(data);
-  }).fail(displayError())
+  $(document).ready(function (){
+
+    $.get(url).done(function(data) {
+      console.log(data);
+      $(".result").html(data);
+    }).fail(displayError())
+  });
 }
+
 
 function displayError() {
   "I'm sorry, there's been an error. Please try again."
